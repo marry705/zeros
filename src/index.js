fe4962b367239b7da57d5e5649ac67169771f10a
Marry705 <marry705chilo@gmail.com>
2018-09-24 19:17:28 +0300

    second

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 module.exports = function getZerosCount(number) {
-  var zero = 0;
-  for(;number>0;){
-      number = Math.floor(number/5); 
-      zero = zero + number;}
+  let zero = 0;
+  let num = number;
+  for(;num>0;){
+      num = Math.floor(num/5); 
+      zero = zero + num;}
   return Math.floor(zero);
 }
